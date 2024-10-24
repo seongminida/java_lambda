b70e90801d1f20d69a6dacf16461ac8ec6b68eab
seongminida <mcytds@naver.com>
2024-10-24 13:15:26 +0900

    Add files via upload

---
+++ LambdaMain4.java
@@ -0,0 +1,80 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import javax.lang.model.type.ArrayType;
+
+public class LambdaMain4 {
+
+   public static void main(String[] args) {
+      
+     
+	   Predicate<String> predicate = (String s) ->{
+		   boolean isTrue = false;
+		   
+		   
+		  isTrue = s.startsWith("A");
+		  return isTrue;
+		   
+		  };
+		  
+		  
+		  System.out.println(predicate.test("Asfdgdgefefdfdf"));
+		  System.out.println(predicate.test("Bsdfdf"));
+      
+		  
+		  //짝수이면 True, 홀수이면 False
+		  Predicate<Integer> predicate2 = (Integer i) -> {
+			  
+			  if(i%2 ==0)
+				  return true;
+			return false;
+		
+		  };
+		  
+		  
+		  System.out.println(predicate2.test(4));
+		  System.out.println(predicate2.test(3));
+		  
+		  
+		  int[] arrNums = {4, 3, 2, 1};
+		  for(int num:arrNums) {
+			  System.out.println("숫자:" + num + "짝수대홀수" + predicate2.test(num));
+			  System.out.println("===============================================");
+			  System.out.println("숫자:" + num + "짝수대홀수" + isEven(num, predicate2));
+		  }
+			  
+		  
+		 List<Integer>list = Arrays.asList(1,5,7,9,11);
+		 
+		 int s;
+		 
+		 s = sum(n->n%2==0,list);
+		 System.out.println("짝수 합" + s);
+		 
+		 s = sum(n->n%2!=0,list);
+		 System.out.println("홀수 합" + s);
+		}
+   
+   		public static int sum(Predicate<Integer> p, List<Integer> list) {
+   			int s = 0;
+   			for(int n : list) {
+   				
+   				if(p.test(n))
+   					s+=n;
+   				}
+   			
+   			return s;
+   		}
+   
+   		public static boolean isEven(int num, Predicate<Integer> p) {
+   			return p.test(num);
+   			
+   			
+   		}
+	
+		
+      
+   }
+
